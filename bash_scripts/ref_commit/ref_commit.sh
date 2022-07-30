6565dbc8bb2f1f01790cebd93cbaebb51785393b
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-07-30 08:46:33 +0300

    Script: add -rmlastnl option to xclip
    From xclip man: when  the  last character of the selection is a newline character, remove it.
    Newline characters that are not the last character in the selection are not affected.

--- bash_scripts/ref_commit/ref_commit.sh
+++ bash_scripts/ref_commit/ref_commit.sh
@@ -4,4 +4,4 @@ commit_hash_src=${1:-"HEAD"}
 url_user_repo="$(git config --get remote.origin.url | sed 's/^.*:\(.*\)\.git/\1/')"
 url_commit_hash="$(git rev-parse $commit_hash_src)"
 url_commit="https://github.com/${url_user_repo}/commit/${url_commit_hash}"
-echo $url_commit | xclip -selection clipboard
+echo $url_commit | xclip -rmlastnl -selection clipboard
